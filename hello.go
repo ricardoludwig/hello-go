6c0ef5ba50fd527f21000555ea61769deb656d60
Ricardo Ludwig <ricardo.ludwig@gmail.com>
2020-06-26 20:34:56 -0300

    -Brincando com Go

---
+++ hello.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func main() {
+	fmt.Println("Hello Word")
+	inferenciaDeTipo()
+
+	fmt.Println("Informe um número para o cálculo do fatorial")
+	var numero int
+	fmt.Scanf("%d", &numero)
+
+	fmt.Println(fatorial(numero))
+}
+
+func inferenciaDeTipo() {
+	versao := 1.1
+	fmt.Println("Este programa está na versão", versao)
+	fmt.Println("Inferindo o tipo da variável float. Há dois tipos de float em GO float32 e float64")
+	fmt.Println("O tipo da variável idade é", reflect.TypeOf(versao))
+}
+
+func fatorial(numero int) int {
+	if (numero > 0) {
+		resultado := numero * fatorial(numero -1)
+		return resultado
+	}
+	return 1
+}
